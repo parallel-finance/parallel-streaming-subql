50a55284c0cf2688e02628c68ad8de69b91dcf38
alfred <alfred.parallel.fi>
2022-04-28 17:13:15 +0800

    fix types of schema

--- src/handlers/executors/stream.ts
+++ src/handlers/executors/stream.ts
@@ -1,80 +1,18 @@
 import { SubstrateEvent } from '@subql/types'
-import { Stream, StreamAccount } from '../../types'
+import { Stream } from '../../types'
 import { convertToAnyChainAddress } from '../utils/address'
 
-const saveStreamAccount = async (blockHeight: number, record, event) => {
+const saveStream = async (blockHeight: number, stream: Stream, event) => {
   try {
-    await record.save()
+    await stream.save()
 
     logger.info(
-      `#${blockHeight} handle ${event} ${JSON.stringify(record)}`
+      `#${blockHeight} handle ${event} ${JSON.stringify(stream)}`
     )
   } catch (error) {
     logger.error(`handle ${event} error: `, error)
   }
 }
-
-// TODO: remove this function as we won't need it.
-const addOutboundStream = async (header, account, id: string) => {
-  // Check sender StreamAccount
-  const currentStreamAccount = await StreamAccount.get(account)
-  if (currentStreamAccount === undefined) {
-    // Create StreamAccount Record
-    const newSenderStreamAccount = StreamAccount.create({
-      id: account,
-      inBound: [],
-      outBound: [id]
-    })
-    await saveStreamAccount(
-      header,
-      newSenderStreamAccount,
-      'Create sender StreamAccount at CreateStream'
-    )
-  } else {
-    currentStreamAccount.outBound.push(id)
-    await saveStreamAccount(
-      header,
-      currentStreamAccount,
-      'Update sender StreamAccount at CreateStream'
-    )
-  }
-}
-
-// TODO: remove this function as we won't need it.
-const addInboundStream = async (header, account, id: string) => {
-  // Check sender StreamAccount
-  const currentStreamAccount = await StreamAccount.get(account)
-  if (currentStreamAccount === undefined) {
-    // Create StreamAccount Record
-    const newReceiverStreamAccount = StreamAccount.create({
-      id: account,
-      inBound: [id],
-      outBound: []
-    })
-    await saveStreamAccount(
-      header,
-      newReceiverStreamAccount,
-      'Create recipient StreamAccount at CreateStream'
-    )
-  } else {
-    currentStreamAccount.inBound.push(id)
-    await saveStreamAccount(
-      header,
-      currentStreamAccount,
-      'Update recipient StreamAccount at CreateStream'
-    )
-  }
-}
-
-type StreamCreatedEventData = [
-  number,
-  string,
-  string,
-  number,
-  number,
-  number,
-  number
-]
 
 export const handleStreamCreated = async (substrateEvent: SubstrateEvent) => {
   const {
@@ -84,34 +22,35 @@ export const handleStreamCreated = async (substrateEvent: SubstrateEvent) => {
     },
     extrinsic
   } = substrateEvent;
-  const [id, sender, recipient, deposit, assetId, startTime, stopTime] = JSON.parse(data.toString()) as StreamCreatedEventData;
+  const [id, sender, recipient, deposit, assetId, startTime, stopTime] = JSON.parse(data.toString()) as [
+    number,
+    string,
+    string,
+    string,
+    number,
+    number,
+    number
+  ];
+
   const createdExtHash: string = extrinsic.extrinsic.hash.toString();
   const blockHeight: number = header.number.toNumber();
   const streamId = id.toString();
   const anyChainSenderAddress: string = convertToAnyChainAddress(sender);
   const anyChainRecipientAddress: string = convertToAnyChainAddress(recipient);
 
-  const ratePerSec: number = deposit / (stopTime - startTime)
-
-  // Create Stream Record
   const streamRecord = Stream.create({
     recipient: anyChainRecipientAddress,
     id: streamId,
     deposit,
     assetId,
-    ratePerSec,
     remainingBalance: deposit,
     sender: anyChainSenderAddress,
-    startTime,
-    stopTime,
+    startTime: new Date(startTime*1000),
+    stopTime: new Date(stopTime*1000),
     createdExtHash
   })
 
-  await saveStreamAccount(blockHeight, streamRecord, 'StreamCreated')
+  logger.info(JSON.stringify(streamRecord));
 
-  // // Add Outbound stream to sender
-  // await addOutboundStream(blockHeight, anyChainSenderAddress, streamId)
-  //
-  // // Add Inbound stream to recipient
-  // await addInboundStream(blockHeight, anyChainRecipientAddress, streamId)
+  await saveStream(blockHeight, streamRecord, 'StreamCreated')
 }
