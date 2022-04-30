289e54317a834beb38124453690fdd7dea240056
alfred <alfred.parallel.fi>
2022-05-01 00:11:44 +0800

    add stream withdrawn

--- src/handlers/executors/stream.ts
+++ src/handlers/executors/stream.ts
@@ -10,7 +10,7 @@ const saveStream = async (blockHeight: number, stream: Stream, event) => {
       `#${blockHeight} handle ${event} ${JSON.stringify(stream)}`
     )
   } catch (error) {
-    logger.error(`handle ${event} error: `, error)
+    logger.error(`handle ${event} error: `, JSON.stringify(error))
   }
 }
 
@@ -26,12 +26,11 @@ export const processStreamCreatedEvent = async (substrateEvent: SubstrateEvent) 
     number,
     string,
     string,
-    bigint,
+    string,
     number,
     number,
     number
   ];
-
   const createdExtHash: string = extrinsic.extrinsic.hash.toString();
   const blockHeight: number = header.number.toNumber();
   const streamId = id.toString();
@@ -51,9 +50,18 @@ export const processStreamCreatedEvent = async (substrateEvent: SubstrateEvent) 
     createdExtHash
   })
 
-  logger.info(JSON.stringify(streamRecord));
-
   await saveStream(blockHeight, streamRecord, 'StreamCreated')
+}
+
+const logStream = (stream: Stream): void =>  {
+  logger.info(`stream.id: ${stream.id} type: ${typeof stream.id}`);
+  logger.info(`stream.recipient: ${stream.recipient} type: ${typeof stream.recipient}`);
+  logger.info(`stream.deposit: ${stream.deposit} type: ${typeof stream.deposit}`);
+  logger.info(`stream.assetId: ${stream.assetId} type: ${typeof stream.assetId}`);
+  logger.info(`stream.remainingBalance: ${stream.remainingBalance} type: ${typeof stream.remainingBalance}`);
+  logger.info(`stream.sender: ${stream.sender} type: ${typeof stream.sender}`);
+  logger.info(`stream.startTime: ${stream.startTime} type: ${typeof stream.startTime}`);
+  logger.info(`stream.stopTime: ${stream.stopTime} type: ${typeof stream.stopTime}`);
 }
 
 export const processStreamWithdrawnEvent = async (substrateEvent: SubstrateEvent) => {
@@ -66,9 +74,12 @@ export const processStreamWithdrawnEvent = async (substrateEvent: SubstrateEvent
   const blockHeight: number = header.number.toNumber();
   logger.info(`process withdrawn event: ${JSON.stringify(substrateEvent)}`);
 
-  const [id,,, amount] = JSON.parse(data.toString()) as [number, string, number, bigint]
+  const [id, , , amount] = JSON.parse(data.toString()) as [number, string, string, string];
   const streamRecord = await Stream.get(id.toString())
-  streamRecord.remainingBalance = streamRecord.remainingBalance - BigInt(amount);
+  if (streamRecord.blockHeight === blockHeight) {
+    return;
+  }
+  streamRecord.remainingBalance = (BigInt(streamRecord.remainingBalance) - BigInt(amount)).toString();
   streamRecord.blockHeight = blockHeight;
   await saveStream(blockHeight, streamRecord, 'StreamWithdrawn')
 }
